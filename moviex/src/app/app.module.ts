9120fd34c27a0fc46b59d00f6d6c796fc35abe59
Mohammed-MFoad <mohammed.mfoad@gmail.com>
2021-03-15 17:46:05 +0200

    add-graphQL-integration-and-get-all-shows

--- moviex/src/app/app.module.ts
+++ moviex/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { GraphQLComponent } from './graph-ql/graph-ql.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphQLComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ moviex/src/app/graph-ql/graph-ql.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Show } from '../shared/show';
+
+@Component({
+  selector: 'app-graph-ql',
+  templateUrl: './graph-ql.component.html',
+  styleUrls: ['./graph-ql.component.scss']
+})
+export class GraphQLComponent implements OnInit {
+  shows: Observable<Show[]>;
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+  ngOnInit(){
+    // this.getShow()
+     this.apollo
+    .watchQuery({
+      query: gql`
+      {
+        shows {
+          name
+          weight
+      }
+    }
+      `,
+    })
+    .valueChanges.subscribe((result: any) => {
+      this.shows = result?.data?.shows;
+      this.loading = result.loading;
+      this.error = result.error;
+    });
+  }
+  ngAfterOnInit() {
+  }
+}

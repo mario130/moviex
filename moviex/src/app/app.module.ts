b8a51c11749e927f30a64dcf09da6057ffdcd183
mario130 <mario.yonan.work@gmail.com>
2021-03-15 21:54:20 +0200

    solve conflicts

--- moviex/src/app/app.module.ts
+++ moviex/src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HeaderComponent } from './components/header/header.component';
-=======
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { GraphQLComponent } from './graph-ql/graph-ql.component';
->>>>>>> 9120fd34c27a0fc46b59d00f6d6c796fc35abe59
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HeaderComponent
-=======
+    HeaderComponent,
     GraphQLComponent
->>>>>>> 9120fd34c27a0fc46b59d00f6d6c796fc35abe59
   ],
   imports: [
     BrowserModule,

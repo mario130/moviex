01f37340d2a24d86f0f0156211f19465e239242b
Mohammed-MFoad <mohammed.mfoad@gmail.com>
2021-03-17 21:11:49 +0200

    add-movies-pagenation-component

---
+++ moviex/src/app/components/movies/movies.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Show } from '../../shared/show';
+import{MoviesPagenationServiceService} from'./services/movies-pagenation-service.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss'],
+})
+export class MoviesComponent implements OnInit {
+  shows:[];
+  // shows: Observable<Show[]>;
+  loading = true;
+  error: any;
+  // pageIndexFromLocalStorage:string;
+   
+  pager: any = {};
+  pagedItems: any[];
+  len : number;
+  
+  constructor(private apollo: Apollo , private moviesService : MoviesPagenationServiceService) {}
+  ngOnInit() {
+    // this.pageIndexFromLocalStorage = localStorage.getItem("pageIndex")
+    // this.getShow()
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            shows {
+              name
+              weight
+              summary
+              image {
+                medium
+              }
+              url
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.shows = result?.data?.shows;
+        console.log(this.shows.length);
+        this.loading = result.loading;
+        this.error = result.error;
+        if (!this.loading) {
+          // if (this.pageIndexFromLocalStorage) {
+          //   this.setPage(this.pageIndexFromLocalStorage)
+          // }
+          // else{
+            this.setPage(1)
+          // }
+        }
+      });
+
+  }
+  setPage(page) {
+    // localStorage.setItem("pageIndex", page);
+    // get pager object from service
+    this.pager = this.moviesService.getPager(this.shows.length, page,12);
+    // get current page of items
+    console.log(this.pager);
+    
+    this.pagedItems = this.shows.slice(
+      this.pager.startIndex,
+      this.pager.endIndex + 1
+    );
+  }
+}

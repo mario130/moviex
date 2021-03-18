ab4e31d038ce1df593fec316b52af29244e0b449
Mohammed-MFoad <mohammed.mfoad@gmail.com>
2021-03-18 02:45:46 +0200

    fix-query-single-show

--- moviex/src/app/components/single-show/single-show.component.ts
+++ moviex/src/app/components/single-show/single-show.component.ts
@@ -9,7 +9,7 @@ import { Apollo, gql } from 'apollo-angular';
 })
 export class SingleShowComponent implements OnInit {
   movieName: string;
-  movieData:Observable<String[]>;
+  movieData:any;
   loading = true;
   error: any;
   overview:string;
@@ -17,6 +17,7 @@ export class SingleShowComponent implements OnInit {
   constructor(private activateRoute: ActivatedRoute, private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.movieData = []
     this.activateRoute.paramMap.subscribe((params: ParamMap) => {
       if (params.get('name') != null) {
         this.movieName = params.get('name')
@@ -32,7 +33,7 @@ export class SingleShowComponent implements OnInit {
         .watchQuery({
           query: gql`
         {
-          show(name:"Penny Dreadful") {
+          show(name: ${this.movieName}) {
           name
          summary
          image{
@@ -43,14 +44,23 @@ export class SingleShowComponent implements OnInit {
         `,
         })
         .valueChanges.subscribe((result: any) => {
-          this.movieData = result?.data?.show.name;
-          this.overview=result?.data?.show.summary;
-          this.overview.replace('</p>','');
-          this.img=result?.data?.show.image.original;
-          console.log(this.overview);
+          // this.movieData = result?.data?.show.name;
+          // this.overview=result?.data?.show.summary;
+          // this.overview.replace('</p>','');
+          // this.img=result?.data?.show.image.original;
+          // console.log(this.overview);
+          this.movieData.push(
+            result?.data?.show.name,
+            result?.data?.show.summary,
+            result?.data?.show.image.original,
+          )
+          // comment From Foad : i declare array and push data in it then print it 
+          console.log( "name : " + this.movieData[0]);
+          console.log( "summary : " + this.movieData[1]);
+          console.log( "image : " + this.movieData[2]);
+          
           this.loading = result.loading;
           this.error = result.error;
-
         });
     })
 

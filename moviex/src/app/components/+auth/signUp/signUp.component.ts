ee905f2d709a12b1835160ebc76c60119b0e7d5e
sabrien96 <sabriensaber0@gmail.com>
2021-03-26 15:41:06 +0200

    apply reactive form in sign up form

--- moviex/src/app/components/+auth/signUp/signUp.component.ts
+++ moviex/src/app/components/+auth/signUp/signUp.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {ConfirmedValidatorService} from '../../../shared/Services/confirmed-validator.service'
 @Component({
   selector: 'app-signUp',
   templateUrl: './signUp.component.html',
   styleUrls: ['./signUp.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+  signUpForm=this.fb.group({
+    username:['',Validators.required],
+    email:['',Validators.required],
+    password:['',Validators.required,Validators.minLength(8)],
+    confirmPassword:['']
+    
+  },
+  {validators:[ConfirmedValidatorService]}
+  );
+  isValidInput(fieldName): boolean {
+    return this.signUpForm.controls[fieldName].invalid &&
+      (this.signUpForm.controls[fieldName].dirty || this.signUpForm.controls[fieldName].touched);
+  }
+  validateRequired(fieldName) {
+    return this.signUpForm.controls[fieldName].errors.required;
+  }
+  validatePattern(fieldName) {
+    return this.signUpForm.controls[fieldName].errors.pattern;
+  }
+  validateLength(fieldName) {
+    return this.signUpForm.controls[fieldName].errors.minLength;
+  }
+  
+  signUp(): void {
+    console.log(this.signUpForm.value);
   }
 
 }
